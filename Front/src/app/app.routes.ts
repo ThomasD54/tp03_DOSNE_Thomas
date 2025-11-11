f1f70704c3580dc32f563226139122caa7e6ea22
Thomas <thomas.dosne@gmail.com>
2025-11-11 23:12:06 +0100

    correction bug de routes

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { ListeUtilisateur } from './liste-utilisateur/liste-utilisateur';
 
 export const routes: Routes = [
   { path: 'formulaire', component: FormulairePollution },
+  { path: 'formulaire-pollution', component: FormulairePollution },
   { path: 'formulaire-utilisateur', component: FormulaireUtilisateur },
   { path: 'liste-utilisateur', component: ListeUtilisateur },
   { path: '**', redirectTo: '' }
