18789dd5c0c8d05dfa542fbcc90b0f450010bcb0
Thomas <thomas.dosne@gmail.com>
2025-11-11 23:55:11 +0100

    add des routes

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
+import { ListePollutionComponent } from './liste-des-pollutions/liste-des-pollutions';
 import { FormulairePollution } from './formulaire-pollution/formulaire-pollution';
 import { FormulaireUtilisateur } from './formulaire-utilisateur/formulaire-utilisateur';
 import { ListeUtilisateur } from './liste-utilisateur/liste-utilisateur';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'accueil', pathMatch: 'full' },
-  { path: 'accueil', component: FormulairePollution },
+  { path: 'accueil', component: ListePollutionComponent },
+  { path: 'pollution/ajouter', component: FormulairePollution },
+  { path: 'pollution/modifier/:id', component: FormulairePollution },
   { path: 'formulaire-utilisateur', component: FormulaireUtilisateur },
   { path: 'liste-utilisateur', component: ListeUtilisateur },
   { path: '**', redirectTo: 'accueil' }
