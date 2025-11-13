877c614c9e86a61f5e741b350d453b45528f016e
Thomas <thomas.dosne@gmail.com>
2025-11-13 14:58:14 +0100

    correction liste user .ts

--- Front/src/app/liste-utilisateur/liste-utilisateur.ts
+++ Front/src/app/liste-utilisateur/liste-utilisateur.ts
@@ -4,10 +4,10 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-liste-utilisateur',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './liste-utilisateur.html',
   styleUrl: './liste-utilisateur.css'
 })
 export class ListeUtilisateur {
-
+  utilisateurs: any[] = [];
 }
