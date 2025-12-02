4f7248bc9e361636afd04819a0fb08f4e7fff022
Thomas <thomas.dosne@gmail.com>
2025-12-02 23:00:45 +0100

    correction fichier route

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 import { FormulairePollution } from './formulaire-pollution/formulaire-pollution';
-import { ListePollutionComponent } from './liste-des-pollutions/liste-des-pollutions';
+import { ListeDesPollutions } from './liste-des-pollutions/liste-des-pollutions';
 import { FormulaireUtilisateur } from './formulaire-utilisateur/formulaire-utilisateur';
 import { ListeUtilisateur } from './liste-utilisateur/liste-utilisateur';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'accueil', pathMatch: 'full' },
-  { path: 'accueil', component: ListePollutionComponent },
+  { path: 'accueil', component: ListeDesPollutions },
   { path: 'pollution/ajouter', component: FormulairePollution },
   { path: 'formulaire-utilisateur', component: FormulaireUtilisateur },
   { path: 'liste-utilisateur', component: ListeUtilisateur },
