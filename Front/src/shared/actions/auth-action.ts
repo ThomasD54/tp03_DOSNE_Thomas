4537f86c82922ae8287e1a66b4b90606e3f29d5c
Thomas <thomas.dosne@gmail.com>
2025-12-03 21:51:43 +0100

    correction

--- Front/src/shared/actions/auth-action.ts
+++ Front/src/shared/actions/auth-action.ts
@@ -1,5 +1,3 @@
-import { AuthConnexion } from '../../shared/actions/auth-action';
-
 export class AuthConnexion {
   static readonly type = '[Auth] Connexion';
   constructor(public payload: { connexion: boolean }) {}
